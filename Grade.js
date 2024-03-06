522dca6c3bedad0b1f840d27eb78aceb7bda3f77
YewIthipunkul <139699878+Yewwys@users.noreply.github.com>
2024-03-06 10:21:12 +0700

    Update js project
    create grading system

---
+++ Grade.js
@@ -0,0 +1,23 @@
+
+
+let score = prompt('Enter your score')
+score = Number(score)
+if (score>=80 && score<=100) {
+    console.log('A')
+} else if (score>=75 && score<80) {
+    console.log('B+')
+} else if (score>=70 && score<75) {
+    console.log('B')
+} else if (score>=65 && score<70) {
+    console.log('C+')
+} else if (score>=60 && score<65) {
+    console.log('C')
+} else if (score>=55 && score<60) {
+    console.log('D+')
+} else if (score>=50 && score<55) {
+    console.log('D')
+} else if (score<50 && score>=0) {
+    console.log('F')
+} else {
+    console.log('Error')
+}-1
