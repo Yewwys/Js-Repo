8855128156cc50cc912b12793008923c0c619173
YewIthipunkul <139699878+Yewwys@users.noreply.github.com>
2024-03-08 14:11:45 +0700

    Learning for loops and array in js

--- loops.js
+++ loops.js
@@ -32,17 +32,71 @@
 // console.log(s)
 
 
-let a = prompt()
-let b = prompt()
-let c = prompt()
+// let a = prompt()
+// let b = prompt()
+// let c = prompt()
 
-a = parseInt(a) ; b = parseInt(b) ; c = parseFloat(c)
+// a = parseInt(a) ; b = parseInt(b) ; c = parseFloat(c)
 
-let s = 0
+// let s = 0
 
-while (a<=b) {
-    s += c**(a) + a -5
-    a += 1
+// while (a<=b) {
+//     s += c**(a) + a -5
+//     a += 1
+// }
+
+// console.log(s)
+
+
+// for (let counter=0; counter<10; counter++) {
+//     console.log('Hello')
+// }
+
+// let m = prompt()
+// let n = prompt()
+
+// m = parseInt(m) ; n = parseInt(n)
+// let sum = 0
+// for (m;m<n;m++) {
+//     if (m%6==0) {
+//         sum += m
+//     } 
+// }
+// console.log(sum)
+
+// let a = prompt()
+// let b = prompt()
+// let r = prompt()
+// let s = 0
+// a = parseInt(a) ; b = parseInt(b) ; r = parseFloat(r)
+// for (a;a<=b;a++) {
+//     s += r**a + a -5
+// }
+// s = s.toFixed(3)
+// console.log(s)
+
+
+// array
+let ages = [30,40,50]
+console.log(ages)
+
+
+// 1.แทนที่
+ages = [45,20]
+console.log(ages)
+
+// 2.ต่อarray
+ages.push('Yew',67)
+console.log(ages)
+
+ages.pop()
+console.log(ages)
+
+if (ages.includes('Yew')) {
+    console.log('You are Yewwys')
 }
 
-console.log(s)
+// sort array
+let num = ['yew','test','logic']
+num.push('ff')
+console.log(num)
